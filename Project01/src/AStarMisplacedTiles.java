0b028ed70e762c98d01e754de83a4b7911cf9974
Khoi Bui <kbui8@uncc.edu>
2019-05-30 22:21:48 -0400

    Proj 01

--- Project01/src/AStarMisplacedTiles.java
+++ Project01/src/AStarMisplacedTiles.java
@@ -1,42 +1,39 @@
 
 public class AStarMisplacedTiles extends AStar {
-
-	int rows = 3;
-	int columns = 3;
 	
-	public AStarMisplacedTiles(Node root, Node goal) {
-		super(root, goal);
+	public AStarMisplacedTiles(Node root, Node goal, int size, int side) {
+		super(root, goal, size, side);
 	}
 
 	@Override
 	public int heuristicFn(State s) {
 		int misplaceValue = 0;
 		int[] currentArray1d = s.getCurrBoard();
-		int[][] currentArray2d = new int[rows][columns];
+		int[][] currentArray2d = new int[side][side];
 
 		int[] goalArray1d = goal.getCurrState().getCurrBoard();
-		int[][] goalArray2d = new int[rows][columns];
+		int[][] goalArray2d = new int[side][side];
 
 		// converting array1d to a 2d array for the goal state
-		for (int x = 0; x < rows; x++) {
-			for (int y = 0; y < columns; y++) {
-				goalArray2d[x][y] = goalArray1d[(x * columns) + y];
+		for (int x = 0; x < side; x++) {
+			for (int y = 0; y < side; y++) {
+				goalArray2d[x][y] = goalArray1d[(x * side) + y];
 			}
 		}
 
 		// converting array1d to a 2d array for the current state
-		for (int x = 0; x < rows; x++) {
-			for (int y = 0; y < columns; y++) {
-				currentArray2d[x][y] = currentArray1d[(x * columns) + y];
+		for (int x = 0; x < side; x++) {
+			for (int y = 0; y < side; y++) {
+				currentArray2d[x][y] = currentArray1d[(x * side) + y];
 			}
 		}
 		
 		//comparing and calculating the state of current node to goal node
 		//will give the total amount of misplace tiles
-		for (int x = 0; x < rows; x++) {
-			for (int y = 0; y < columns; y++) {
-				for (int z = 0; z < rows; z++) {
-					for (int j = 0; j < columns; j++) {
+		for (int x = 0; x < side; x++) {
+			for (int y = 0; y < side; y++) {
+				for (int z = 0; z < side; z++) {
+					for (int j = 0; j < side; j++) {
 						if (currentArray2d[x][y] == goalArray2d[z][j]) {
 							if(Math.abs(x - z) + Math.abs(y - j) != 0) {
 								misplaceValue++; 
